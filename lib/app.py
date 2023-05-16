31d94f7450c39270f18c6dcb3b76d8c44be2a78e
Job koech <jobkoech610@gmail.com>
2023-05-16 15:35:33 +0300

    completed assignment

---
+++ lib/app.py
@@ -0,0 +1,15 @@
+# first python line of code!.
+
+#using print() to check the output
+# print("Hello world!")
+# print("Hello sun!")
+# print("Hello sea!")
+
+#using end=" " to choose own print() ending
+# print("Hello world!", end=" ")
+# print("Hello sun!", end="!! ")
+# print("Hello sea!", end="!!!\n")
+
+print("Hello World! Pass this test, please.")
+
+
